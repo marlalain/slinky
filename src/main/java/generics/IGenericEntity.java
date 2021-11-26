7285b59b7f7316a2c503b0f88f5fc2d76ecfd682
Paulo Elienay <paulo@pauloelienay.com>
2021-11-26 05:58:25 -0300

    feat(IGenericEntity): adds `isUpdate` checker

--- src/main/java/generics/IGenericEntity.java
+++ src/main/java/generics/IGenericEntity.java
@@ -2,7 +2,13 @@ package generics;
 
 import java.io.Serializable;
 
+import static java.util.Objects.isNull;
+
 @SuppressWarnings({"unused"})
 public interface IGenericEntity<T, S extends Serializable> {
 	S getId();
+
+	default boolean isUpdate() {
+		return isNull(getId());
+	}
 }
