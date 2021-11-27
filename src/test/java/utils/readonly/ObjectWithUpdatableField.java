a199b5ecde0c6e736993cf604c0b040d042b81c1
Paulo Elienay <paulo@pauloelienay.com>
2021-11-27 14:08:37 -0300

    test(ReflectionUtilTest): adds more tests

--- src/test/java/utils/readonly/ObjectWithUpdatableField.java
+++ src/test/java/utils/readonly/ObjectWithUpdatableField.java
@@ -1,6 +1,12 @@
 package utils.readonly;
 
+import lombok.AllArgsConstructor;
+
+import javax.persistence.Column;
+
 @SuppressWarnings({"unused"})
+@AllArgsConstructor
 public class ObjectWithUpdatableField {
+	@Column
 	private String name;
 }
