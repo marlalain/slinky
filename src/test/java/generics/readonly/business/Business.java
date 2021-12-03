db09ae6c38d363ba7e88fe874f02658c5954c23f
Paulo Elienay <paulo@pauloelienay.com>
2021-12-02 23:19:36 -0300

    test: full reverts business tests

--- src/test/java/generics/readonly/business/Business.java
+++
@@ -1,9 +0,0 @@
-package generics.readonly.business;
-
-import entities.DummyEntity;
-import generics.GenericBusiness;
-import org.springframework.stereotype.Service;
-
-@Service
-public class Business extends GenericBusiness<DummyEntity, Long> {
-}
--- src/test/java/generics/readonly/business/MultipleFieldsEntityBusiness.java
+++
@@ -1,9 +0,0 @@
-package generics.readonly.business;
-
-import entities.DummyEntityWithMultipleFields;
-import generics.GenericBusiness;
-import org.springframework.stereotype.Service;
-
-@Service
-public class MultipleFieldsEntityBusiness extends GenericBusiness<DummyEntityWithMultipleFields, Long> {
-}
