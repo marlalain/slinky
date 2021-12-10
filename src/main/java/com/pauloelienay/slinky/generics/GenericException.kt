76a1252fca34b7898ed18cb515b76917c84363ef
Paulo Elienay <paulo@pauloelienay.com>
2021-12-09 22:48:15 -0300

    refactor(generics): moves GenericException.kt

--- src/main/java/com/pauloelienay/slinky/generics/GenericException.kt
+++ src/main/java/com/pauloelienay/slinky/generics/GenericException.kt
@@ -1,4 +1,4 @@
-package com.pauloelienay.slinky.exceptions
+package com.pauloelienay.slinky.generics
 
 import org.springframework.http.HttpStatus
 import org.springframework.web.bind.annotation.ResponseStatus
