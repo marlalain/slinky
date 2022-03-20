a487e36c82505ca28c9ef9fb19903a08f8b50940
Paulo Elienay <pauloalbuquerque@acm.org>
2022-03-20 16:59:38 -0300

    test: cleans up test class
    Signed-off-by: Paulo Elienay <paulo@pauloelienay.com>

--- src/test/java/EntityUtilsTest.java
+++ src/test/java/EntityUtilsTest.java
@@ -1,44 +1,43 @@
-import org.junit.Assert;
 import org.junit.Test;
 import utils.EntityUtils;
 
-import java.util.Objects;
+import static org.junit.Assert.*;
 
 public class EntityUtilsTest {
-	@SuppressWarnings({"InstantiationOfUtilityClass", "ConstantConditions"})
+	@SuppressWarnings({"InstantiationOfUtilityClass"})
 	@Test
 	public void smokeTest() {
 		EntityUtils utils = new EntityUtils();
-		Assert.assertTrue(Objects.nonNull(utils));
+		assertNotNull(utils);
 	}
 
 	@Test
 	public void shouldSayNullIsNull() {
-		Assert.assertTrue(EntityUtils.isNull(null));
+		assertTrue(EntityUtils.isNull(null));
 	}
 
 	@Test
 	public void shouldSayNullIdIsNull() {
 		DummyEntity entity = new DummyEntity();
-		Assert.assertTrue(EntityUtils.isNull(entity));
+		assertTrue(EntityUtils.isNull(entity));
 	}
 
 	@Test
 	public void shouldSayNonNullIsntNull() {
 		DummyEntity entity = new DummyEntity();
 		entity.setId(1L);
-		Assert.assertFalse(EntityUtils.isNull(entity));
+		assertFalse(EntityUtils.isNull(entity));
 	}
 
 	@Test
 	public void shouldSayNonNullIsNonNull() {
 		DummyEntity entity = new DummyEntity();
 		entity.setId(1L);
-		Assert.assertTrue(EntityUtils.nonNull(entity));
+		assertTrue(EntityUtils.nonNull(entity));
 	}
 
 	@Test
 	public void shouldSayNullIsntNonNull() {
-		Assert.assertFalse(EntityUtils.nonNull(null));
+		assertFalse(EntityUtils.nonNull(null));
 	}
 }
